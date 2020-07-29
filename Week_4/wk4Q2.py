b85a99a82db28b6f3c868214b49e38d5fdbb56cd
JoshFlack <W9489162@live.tees.ac.uk>
2020-07-29 19:12:36 +0100

    Create wk4Q2.py

---
+++ Week_4/wk4Q2.py
@@ -0,0 +1,49 @@
+#Josh Flack
+#Date
+#Name of proram
+
+
+import matplotlib.pyplot as plt
+from random import randint
+import random as ran
+import math as ma
+
+
+class mathengine:
+    name = ""
+    def __init__(self, name): 
+        self.num = num
+        num = 45
+
+    def add(self,add):
+        x = int(input("Please enter a number"))
+        ans1 = (x + num)
+        return (ans1)
+
+    def sub(self,add):
+        x = int(input("Please enter a number"))
+        ans2 = (x - num)
+        return (ans2)
+
+    def mult(self,add):
+        x = int(input("Please enter a number"))
+        ans3 = (x * num)
+        return (ans3)
+
+    def div(self,add):
+        x = int(input("Please enter a number"))
+        ans4 = (x / num)
+        return (ans4)
+
+        
+
+#    def greet_to_file(self,filename):
+#        with open(filename, 'w') as file: 
+#            file.write("hello" + self.name)
+        
+
+#greeter = Greeter("Josh")
+
+#greeter.greet_to_file("hi.txt")
+
+#greeter.greet_to_screen()
