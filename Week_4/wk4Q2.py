214306a7a832feab7b5c09696a3504e8ac8ae87f
JoshFlack <W9489162@live.tees.ac.uk>
2020-07-29 19:42:57 +0100

    Update wk4Q2.py

--- Week_4/wk4Q2.py
+++ Week_4/wk4Q2.py
@@ -11,30 +11,31 @@ import math as ma
 
 class mathengine:
     name = ""
-    def __init__(self, name): 
+    def __init__(self, num): 
         self.num = num
-        num = 45
 
     def add(self,add):
-        x = int(input("Please enter a number"))
-        ans1 = (x + num)
-        return (ans1)
+        self.num = self.num + add
+        return self.num
 
-    def sub(self,add):
-        x = int(input("Please enter a number"))
+    def sub(self,sub):
+        #x = int(input("Please enter a number"))
         ans2 = (x - num)
         return (ans2)
 
-    def mult(self,add):
-        x = int(input("Please enter a number"))
+    def mult(self,mult):
+        #x = int(input("Please enter a number"))
         ans3 = (x * num)
         return (ans3)
 
-    def div(self,add):
-        x = int(input("Please enter a number"))
+    def div(self,div):
+       # x = int(input("Please enter a number"))
         ans4 = (x / num)
         return (ans4)
 
+
+m_eng = mathengine(45)
+m_eng.add(5)
         
 
 #    def greet_to_file(self,filename):
