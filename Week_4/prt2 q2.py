ff23a95fa21133a1e9c8825dd5da498b7c2fa4a4
JoshFlack <W9489162@live.tees.ac.uk>
2020-07-30 15:18:31 +0100

    update 1

--- Week_4/prt2 q2.py
+++ Week_4/prt2 q2.py
@@ -9,22 +9,27 @@ import random as ran
 import math as ma
 
 
-def blocked:
+def blocked():
     with open("blocked_words.txt", mode="r") as my_file:
-         blocked_words[]
+         for line in my_file:
+             blocked_words = []
 
 while True:
 
-    url = input("Enter address to scan: ")
+    url = input("Enter address to scan: (enter Exit to quit")
     response = urldl.urlopen(url)
+    if url == Exit:
+        break
+        
 
     data = response.read()
     txt = data.decode().lower()
 
-    if blocked_words[] in txt:
+    if blocked_words in txt:
         print ("This bage is blocked ")
 
     else:
+        ("This page is ok to look at")
         
 
     
