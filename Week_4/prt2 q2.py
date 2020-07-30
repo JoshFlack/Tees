27c5bfbfc45c6ada6851f40cac306490682499f4
JoshFlack <W9489162@live.tees.ac.uk>
2020-07-30 15:09:20 +0100

    prt2 q1 &2

---
+++ Week_4/prt2 q2.py
@@ -0,0 +1,30 @@
+#Josh Flack
+#Date
+#Name of proram
+
+import urllib.request as urldl
+import matplotlib.pyplot as plt
+from random import randint
+import random as ran
+import math as ma
+
+
+def blocked:
+    with open("blocked_words.txt", mode="r") as my_file:
+         blocked_words[]
+
+while True:
+
+    url = input("Enter address to scan: ")
+    response = urldl.urlopen(url)
+
+    data = response.read()
+    txt = data.decode().lower()
+
+    if blocked_words[] in txt:
+        print ("This bage is blocked ")
+
+    else:
+        
+
+    
